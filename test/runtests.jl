264fd9be8fd3071c15fa7e2bb9cae58ced3ba605
Steven G. Johnson <stevenj@mit.edu>
2014-09-12 15:58:43 -0400

    added tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,4 +2,11 @@ using LaTeXStrings
 using Base.Test
 
 # write your own tests here
-@test 1 == 1
+tst1 = L"an equation: $\alpha^2$"
+tst1u = utf8(tst1)
+@test tst1 == "an equation: \$\\alpha^2\$" == tst1u
+@test L"\alpha^2" == "\$\\alpha^2\$"
+@test stringmime("text/latex", tst1) == tst1u
+buf = IOBuffer()
+writemime(buf, "text/latex", tst1)
+@test takebuf_string(buf) == tst1u

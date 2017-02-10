8d209344d4c49c4d8b8e5841d5c591f1acb6ab31
Yichao Yu <yyc1992@gmail.com>
2017-02-10 00:11:04 -0500

    Fix test depwarn on 0.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,9 +7,7 @@ tst1u = Compat.UTF8String(tst1)
 @test tst1 == "an equation: \$\\alpha^2\$" == tst1u
 @test L"\alpha^2" == "\$\\alpha^2\$"
 @test stringmime("text/latex", tst1) == tst1u
-buf = IOBuffer()
-@compat show(buf, "text/latex", tst1)
-@test takebuf_string(buf) == tst1u
+@test sprint(show, "text/latex", tst1) == tst1u
 @test latexstring(tst1u) == tst1 == LaTeXString(tst1u)
 
 @test ccall(:strlen, Csize_t, (Cstring,), tst1) == ccall(:strlen, Csize_t, (Ptr{UInt8},), tst1) == sizeof(tst1)
